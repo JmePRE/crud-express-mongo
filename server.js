5e1c4157b91d8d1e12b073a5e2127774edc09466
JmePRE <jaime.pang.rei.ern@gmail.com>
2020-10-20 16:18:06 +0800

    Connect to atlas database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //Imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const app = express();
+const mongodb = require('mongodb').MongoClient;
 
 //body-parser
 //tells body-parser to extract data from the <form> element and add them to the 
@@ -13,18 +15,39 @@ app.listen(4000, function() {
     console.log("listening on port 4000");
 })
 
-//CRUD Methods----------------------------------------------------------------
+//establish connection
+//uname: root, pwd: yodafoundation
+var connectionString = 'mongodb+srv://root:yodafoundation@cluster0.cldb8.mongodb.net/test?retryWrites=true&w=majority';
 
-//GET request
-app.get('/', (req, res) => {
-    //res.send("Hello World")
-    res.sendFile(__dirname + "/index.html")
+/* 
+MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.error(err);
+    console.log('Connected to database')
 })
+*/
 
-//POST request
-app.post('/quotes', (req,res) => {
-    console.log(req.body);
-    //res.send("post received")
-})
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+
+    //CRUD Methods----------------------------------------------------------------
+    //GET request
+    app.get('/', (req, res) => {
+        //res.send("Hello World")
+        res.sendFile(__dirname + "/index.html")
+    })
+
+    //POST request
+    app.post('/quotes', (req,res) => {
+        console.log(req.body);
+        //res.send("post received")
+    })
+
+
+  })
+
+
+
 
 //start with nodemon server.js
